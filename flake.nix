475d08b31fa34b59b07bfe1d32735c06fdd00e0a
Rakarake <raka@rakarake.xyz>
2026-04-28 16:23:21 +0200

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "kwm - kewuaa's Window Manager";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        defaultPackage = pkgs.stdenv.mkDerivation rec {
+          name = "kwm";
+          src = ./.;
+
+          dontSetZigDefaultFlags = true;
+          zigBuildFlags = [
+            "--system"
+            "${deps}"
+            "-Doptimize=ReleaseSafe"
+          ];
+
+          deps = pkgs.callPackage ./deps.nix { };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            wayland-scanner
+            wayland-protocols
+            zig_0_15
+          ];
+
+          buildInputs = with pkgs; [
+            wayland
+            libxkbcommon
+            pixman
+            fcft
+          ];
+        };
+
+        devShell = pkgs.mkShell {
+          packages =
+            defaultPackage.nativeBuildInputs ++
+            defaultPackage.buildInputs ++ [
+              # Use `zon2nix > deps.nix` to generate dependencies.
+              pkgs.zon2nix
+            ];
+        };
+      }
+    );
+}
